74aa4ffd9c27d588fd8252045e931cc91043c271
Niels <7136117+TheDutchDev@users.noreply.github.com>
2023-09-17 17:45:42 +0200

    Renamed some stuff. made chat working properly with default altV chat resource

---
+++ ChatExtensions.cs
@@ -0,0 +1,22 @@
+﻿using AltV.Net;
+using AltV.Net.Elements.Entities;
+
+namespace AltV.Icarus.Chat;
+
+public static class ChatExtensions
+{
+    public static void SendChatMessage( this IPlayer player, string message )
+    {
+        player.Emit( ChatModule.EventName, null, message );
+    }
+    
+    public static void SendChatMessageToAll( this IPlayer sender, string message )
+    {
+        Alt.EmitAllClients( ChatModule.EventName, sender.Name, message );
+    }
+    
+    public static void SendChatMessageToAll( string message )
+    {
+        Alt.EmitAllClients( ChatModule.EventName, null, message );
+    }
+}
--- Interfaces/IChat.cs
+++ Interfaces/IChat.cs
@@ -5,12 +5,8 @@ namespace AltV.Icarus.Chat;
 public delegate void ChatMessageDelegate( IPlayer player, string message );
 public delegate Task ChatMessageAsyncDelegate( IPlayer player, string message );
 
-public interface IChatManager
+public interface IChat
 {
     event ChatMessageDelegate? OnChatMessage;
     event ChatMessageAsyncDelegate? OnChatMessageAsync;
-
-    void SendMessageToPlayer( IPlayer player, string message );
-    void SendMessageToAll( IPlayer sender, string message );
-    void SendMessageToAll( string message );
 }
