8d7d7b67e8adb7c38ff8963a6d12ab40f5ea57d3
Niels <7136117+TheDutchDev@users.noreply.github.com>
2023-11-12 12:28:38 +0100

    Fix workflow. add more docs.

--- src/ChatExtensions.cs
+++ src/ChatExtensions.cs
@@ -3,18 +3,35 @@ using AltV.Net.Elements.Entities;
 
 namespace AltV.Icarus.Chat;
 
+/// <summary>
+/// Contains some extension methods for the chat module
+/// </summary>
 public static class ChatExtensions
 {
+    /// <summary>
+    /// Send a chat message to the specified player
+    /// </summary>
+    /// <param name="player">The player to send the message to</param>
+    /// <param name="message">The message to send</param>
     public static void SendChatMessage( this IPlayer player, string message )
     {
         player.Emit( ChatModule.EventName, null, message );
     }
     
+    /// <summary>
+    /// Sends the specified message to all players
+    /// </summary>
+    /// <param name="sender">The player who sends the message</param>
+    /// <param name="message">The message to send</param>
     public static void SendChatMessageToAll( this IPlayer sender, string message )
     {
         Alt.EmitAllClients( ChatModule.EventName, sender.Name, message );
     }
     
+    /// <summary>
+    /// Sends the specified message to all players
+    /// </summary>
+    /// <param name="message">The message to send</param>
     public static void SendChatMessageToAll( string message )
     {
         Alt.EmitAllClients( ChatModule.EventName, null, message );
